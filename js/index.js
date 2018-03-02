b70e9521df3a47bf1152fdf26a0f1245beea7849
Unknown <avtar.singh@siu.edu>
2018-03-02 00:34:32 -0600

    add to DB function now working

--- js/index.js
+++ js/index.js
@@ -2,37 +2,27 @@ var mainText = document.getElementById("mainText");
 var submitButton = document.getElementById("submitButton");
 var firebaseRef = firebase.database().ref();
 
-
-//code to add parking  spots manually 
-function addSpot(event) {
-    if (event.which == 13 || event.keyCode == 13) { 
-        // as the user presses the enter key, we will attempt to save the data
-        var soptNum = document.getElementById('soptNum').value.trim();
-        if (soptNum.length > 0) {
-        	var li = '<li>' + addSpot + '</li>';
-            document.getElementById('parkingSpots').innerHTML += li;
+//test function to just populate html list no DB intraction
+function saveToList(event) {
+    if (event.which == 13 || event.keyCode == 13) { // as the user presses the enter key, we will attempt to save the data
+        var spotNumber = document.getElementById('spotNumber').value.trim();
+        if (spotNumber.length > 0) {
+            saveToFB(spotNumber);
+        	var li = '<li>' + spotNumber + '</li>';
+            document.getElementById('allSpots').innerHTML += li;
+            
         }
-        document.getElementById('soptNum').value = '';
+        document.getElementById('spotNumber').value = '';
         return false;
     }
 }
 
-function submitClick(){
-	
-	firebaseRef.child("SIU Lot A").child("Spot 102").set({
-		Spot1: "Open",
-		Spot2: "Occupied",
-		Spot3: "Open",
-		Spot4: "Open"
+//code to add spot to DB
+function saveToFB(spotNumber){	
+	firebaseRef.child("SIU Lot A").child(spotNumber).set({
+		Occupited: "True",
+		TimeRem: "02:00:00",
+        PaidOn: "02/08/2018 12:00"
 	});
-	
-	
-	
-}
-
-function newClick() {
-	var messageText = mainText.value;
-	
-	firebaseRef.getRef().child("Users").child("Username").remove();
-	firebaseRef.child("Users").child("New").set(messageText);
+		
 }

ecbff0be6a7b7faf24c81292796accb642d8c485
Unknown <avtar.singh@siu.edu>
2018-03-01 23:19:53 -0600

    Function to add spots to DB

--- js/index.js
+++ js/index.js
@@ -2,6 +2,21 @@ var mainText = document.getElementById("mainText");
 var submitButton = document.getElementById("submitButton");
 var firebaseRef = firebase.database().ref();
 
+
+//code to add parking  spots manually 
+function addSpot(event) {
+    if (event.which == 13 || event.keyCode == 13) { 
+        // as the user presses the enter key, we will attempt to save the data
+        var soptNum = document.getElementById('soptNum').value.trim();
+        if (soptNum.length > 0) {
+        	var li = '<li>' + addSpot + '</li>';
+            document.getElementById('parkingSpots').innerHTML += li;
+        }
+        document.getElementById('soptNum').value = '';
+        return false;
+    }
+}
+
 function submitClick(){
 	
 	firebaseRef.child("SIU Lot A").child("Spot 102").set({

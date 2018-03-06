fd0d71901b3713cfe5fe0da05e4b9e585a0f140c
Unknown <avtar.singh@siu.edu>
2018-03-05 21:24:19 -0600

    Ability to add Lot and Parking spot

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ var mainText = document.getElementById("mainText");
 var submitButton = document.getElementById("submitButton");
 var firebaseRef = firebase.database().ref();
 
+var lotsRef = firebaseRef.child("Lots");
+var lotARef = lotsRef.child("SIU Lot A");
+
+
+
 //test function to just populate html list no DB intraction
 function saveToList(event) {
     if (event.which == 13 || event.keyCode == 13) { // as the user presses the enter key, we will attempt to save the data
@@ -9,7 +14,7 @@ function saveToList(event) {
         if (spotNumber.length > 0) {
             saveToFB(spotNumber);
         	var li = '<li>' + spotNumber + '</li>';
-            document.getElementById('allSpots').innerHTML += li;
+            document.getElementById('lotASpots').innerHTML += li;
             
         }
         document.getElementById('spotNumber').value = '';
@@ -17,12 +22,15 @@ function saveToList(event) {
     }
 }
 
-//code to add spot to DB
+//code to add spot to DB by lot reffrence 
 function saveToFB(spotNumber){	
-	firebaseRef.child("SIU Lot A").child(spotNumber).set({
+    var inLot = document.getElementById('lot').value.trim();
+	lotsRef.child(inLot).child(spotNumber).set({
 		Occupited: "True",
 		TimeRem: "02:00:00",
         PaidOn: "02/08/2018 12:00"
 	});
 		
 }
+
+

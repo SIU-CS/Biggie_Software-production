984df40a2aa385dff11f122ad4959706fac52980
Unknown <avtar.singh@siu.edu>
2018-03-06 10:04:40 -0600

    Added currunt timestamp to stalls

--- js/index.js
+++ js/index.js
@@ -27,10 +27,42 @@ function saveToFB(spotNumber){
     var inLot = document.getElementById('lot').value.trim();
 	lotsRef.child(inLot).child(spotNumber).set({
 		Occupited: "True",
-		TimeRem: "02:00:00",
-        PaidOn: "02/08/2018 12:00"
+		TimeRem: "2:00:00",
+        PaidOn: timeStamp()
 	});
 		
+}
+
+
+//create time stamp 
+function timeStamp() {
+// Create a date object with the current time
+  var now = new Date();
+
+// array with the current month, day and time
+  var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+
+// array with the current hour, minute and second
+  var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+
+// find AM or PM depending on the hour
+  var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+
+// Convert hour from military time
+  time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+
+// If hour is 0, set it to 12
+  time[0] = time[0] || 12;
+
+// If seconds and minutes are less than 10, add a zero
+  for ( var i = 1; i < 3; i++ ) {
+    if ( time[i] < 10 ) {
+      time[i] = "0" + time[i];
+    }
+  }
+
+// Return the formatted string
+  return date.join("/") + " " + time.join(":") + " " + suffix;
 }
 
 

fd99699aebb3e9d459ebd04fb983d34c097344d2
Zachary McGee <zachary.mcgee@siu.edu>
2018-04-28 17:15:35 -0500

    Animate Mobile Sidebar Open and Close

--- js/mobile_sidebar.js
+++ js/mobile_sidebar.js
@@ -4,10 +4,38 @@ function openMenu()
     document.getElementById('sidebarMenu').setAttribute("class", "sidebarOpen");
     document.getElementById('sidebar').setAttribute("class", "sidebarOpen");
     document.getElementById('textcenter').setAttribute("class", "text-center-open");
+
+    var textcenter = document.getElementById("textcenter");
+    var sidebar = document.getElementById("sidebar");
+    var pos = 0;
+    var id = setInterval(frame, 1);
+    function frame() {
+      if (pos == 160) {
+        sidebar.style.display = none;
+        clearInterval(id);
+      } else {
+        pos = pos + 5;
+        textcenter.style.marginLeft = pos + 'px';
+        sidebar.style.width = pos + 'px';
+      }
+    }
   }
   else {
-    document.getElementById('sidebarMenu').setAttribute("class", "sidebar");
-    document.getElementById('sidebar').setAttribute("class", "sidebar");
-    document.getElementById('textcenter').setAttribute("class", "text-center");
+    var textcenter = document.getElementById("textcenter");
+    var sidebar = document.getElementById("sidebar");
+    var pos = 160;
+    var id = setInterval(frame, 1);
+    function frame() {
+      if (pos == 0) {
+        document.getElementById('sidebarMenu').setAttribute("class", "sidebar");
+        document.getElementById('sidebar').setAttribute("class", "sidebar");
+        document.getElementById('textcenter').setAttribute("class", "text-center");
+        clearInterval(id);
+      } else {
+        pos = pos - 5;
+        textcenter.style.marginLeft = pos + 'px';
+        sidebar.style.width = pos + 'px';
+      }
+    }
   }
 }

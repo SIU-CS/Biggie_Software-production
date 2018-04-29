441e1b596c86d44f25e254a32dde56d2204bf500
Unknown <avtar.singh@siu.edu>
2018-04-29 14:18:07 -0500

    minor change

--- js/addTime.js
+++ js/addTime.js
@@ -1,8 +1,6 @@
-var mainText = document.getElementById("mainText");
-var submitButton = document.getElementById("submitButton");
 
-var firebaseRef = firebase.database().ref();
-var lotsRef = firebaseRef.child("Lots");
-var lotARef = lotsRef.child("SIU Lot A/");
-var lotBRef = lotsRef.child("SIU Lot B/");
-var spot;
+function removeAddBtn() {
+    window.alert("sometext");
+    document.getElementsByClassName("addTimeToSpot").style.display = 'none';
+}
+    

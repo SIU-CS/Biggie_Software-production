f4bf1c52a086941d5dd0742bda412180e18e318c
Unknown <avtar.singh@siu.edu>
2018-05-01 21:34:10 -0500

    AddTime btn only visible if timer is active

--- js/home.js
+++ js/home.js
@@ -13,8 +13,9 @@ firebase.database().ref('/USERS_TABLE/' + guid + '/CURRENT_SPOT').once('value').
   amount = minutesToMillis(amount); // Change amount paid for to milliseconds
     //spot.remove();
   if((amount - diff) < 0){
+      document.getElementById('addTimeSpot').style.display = 'none';
     document.getElementById('cardSpotTimeLot').innerHTML = lot + " " + spot + " " + "Time Expired!";
-      
+    
   }
   else {
     var timeLeft = Math.abs(amount - diff); // Get the difference

185522055ea46858469c7768776b25ef193a4b0f
Zachary McGee <zachary.mcgee@siu.edu>
2018-05-03 01:24:04 -0500

    Admin - Add and Remove Spots

--- admin/js/spots.js
+++ admin/js/spots.js
@@ -19,21 +19,22 @@ function refreshList() {
             var timerem = childSnapshot.val().TimeRem;
 
             var d1 = new Date(); // Get the time right now
+            d1 = d1.toISOString().substr(11, 8);
+            d1 = minutesToMillis(d1);
             var d2 = new Date(timeStamp); // Get the purchase time
-            var diff = Math.abs(d1-d2); // Minus those
-
-            amount = minutesToMillis(amount); // Change amount paid for to milliseconds
-
+            d2 = d2.toISOString().substr(11, 8);
+            d2 = minutesToMillis(d2);
+            var diff = (d1 - d2); // Minus those
             timerem = minutesToMillis(timerem); // Change amount paid for to milliseconds
-
            // window.alert(key  + " "+(timerem + diff)); //test alert
 
-            if ((timerem - diff) < 0) {
-                var spotRemoved = lotARef.child(key);
-                spotRemoved.remove();
+            if((timerem - diff) > 0){
+              var entry = '<tr><td>' + key + '</td><td>' + timeStamp + '</td><td>' + 'Time Left: ' + secondsToTime(timerem - diff) + '</td></tr>';
             }
-
-            var entry = '<tr><td>' + key + '</td><td>' + timeStamp + '</td><td>' + timerem + '</td></tr>';
+            else
+            {
+              var entry = '<tr><td>' + key + '</td><td>' + timeStamp + '</td><td>' + 'EXPIRED' + '</td></tr>';
+            }
             document.getElementById('tbody').innerHTML += entry;
         });
     });
@@ -57,13 +58,13 @@ function refreshList() {
             var diff = (d1 - d2); // Minus those
             timerem = minutesToMillis(timerem); // Change amount paid for to milliseconds
 
-            if ((timerem - diff) < 0) {
-                var spotRemoved = lotBRef.child(key);
-                spotRemoved.remove();
+            if((timerem - diff) > 0){
+              var entry = '<tr><td>' + key + '</td><td>' + timeStamp + '</td><td>' + 'Time Left: ' + secondsToTime(timerem - diff) + '</td></tr>';
             }
-
-            var entry = '<tr><td>' + key + '</td><td>' + timeStamp + '</td></tr>';
-
+            else
+            {
+              var entry = '<tr><td>' + key + '</td><td>' + timeStamp + '</td><td>' + 'EXPIRED' + '</td></tr>';
+            }
             document.getElementById('rbody').innerHTML += entry;
         });
     });
@@ -76,4 +77,51 @@ function minutesToMillis(minutes) {
     var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
 
     return seconds;
+}
+
+function secondsToTime(seconds) {
+  var date = new Date(1970,0,1);
+  date.setSeconds(seconds);
+  return date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+}
+
+function showAddSpot(lot){
+  console.log("spots" + lot + "option");
+  document.getElementById("spots" + lot + "option").innerHTML = '<br><div class="formheader"><p>Add Spot</p></div><div class="account-info"><p>Spot:</p><input id="newspot" type="text" class="account-form" name="fname"></div><br><button onClick="addSpot(\'' + lot + '\')" class="btn btn-default" type="button" id="quickstart-sign-up" name="updateinfo">Add</button><button onClick="cancelSpotOption()" class="btn btn-default" type="button" id="quickstart-sign-up" name="updateinfo">Cancel</button></div>';
+}
+
+function showDeleteSpot(lot){
+  document.getElementById("spots" + lot + "option").innerHTML = '<br><div class="formheader"><p>Delete Spot</p></div><div class="account-info"><p>Spot:</p><input id="deletespot" type="text" class="account-form" name="fname"></div><br><button onClick="deleteSpot(\'' + lot + '\')" class="btn btn-default" type="button" id="quickstart-sign-up" name="updateinfo">Delete</button><button onClick="cancelSpotOption()" class="btn btn-default" type="button" id="quickstart-sign-up" name="updateinfo">Cancel</button></div>';
+}
+
+function addSpot(lot){
+  var rootRef = firebase.database().ref();
+  var spotsRef = rootRef.child('/Lots/SIU Lot ' + lot + '/' + document.getElementById("newspot").value);
+  var now = new Date();
+  spotsRef.set({
+      "PaidOn": "1/1/2011 11:11:11 PM",
+      "TimeRem": "0:00:00"
+  });
+  showSuccess("Spot: " + document.getElementById("newspot").value + " Added!")
+}
+
+function deleteSpot(lot){
+  var rootRef = firebase.database().ref();
+  var spotsRef = rootRef.child('/Lots/SIU Lot ' + lot + '/' + document.getElementById("deletespot").value);
+  var now = new Date();
+  spotsRef.remove();
+  showSuccess("Spot: " + document.getElementById("deletespot").value + " Removed!")
+}
+
+function cancelSpotOption() {
+  document.getElementById("spotsoption").innerHTML = '';
+}
+
+function refreshPage(){
+  location.reload();
+}
+
+function showSuccess(message){
+  document.getElementById("spotstable").innerHTML = message + '<br><button onClick="refreshPage()" class="btn btn-default" type="button" id="quickstart-sign-up" name="updateinfo">Continue</button>';
+  document.getElementById("spotstableb").innerHTML = '';
 }
